f02f127c8e4a27e02d627649bd8d79b9a322cd4b
xiaobai497 <dusx0829@163.com>
2026-05-27 14:11:40 +0800

    feat:初步合Excel和Canvas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,20 +3,13 @@ import zhCN from 'antd/locale/zh_CN'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { Provider } from 'react-redux'
 import 'antd/dist/reset.css'
 import App from '@/App.tsx'
 import '@/index.css'
-import { store } from '@/spreadsheet/store'
 import { store } from '@/spreadsheet/store/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <ConfigProvider locale={zhCN}>
-        <App />
-      </ConfigProvider>
-    </Provider>
     <Provider store={store}>
       <ConfigProvider locale={zhCN}>
         <App />
